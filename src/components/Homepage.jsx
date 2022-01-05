3c995e417e28deafc39447febcd6b08d75ebaa33
Sean Monaghan <smonagha@conncoll.edu>
2022-01-05 16:02:26 -0500

    added buttons for changing state of attendance

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,20 +2,23 @@ import React from "react";
 
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-
-import isWeekend from 'date-fns/isWeekend';
+import { Grid } from "@mui/material";
+import { Button, ButtonGroup } from "@mui/material";
 import TextField from '@mui/material/TextField';
 import StaticDatePicker from '@mui/lab/StaticDatePicker';
 import homepagecss from "../components/homepage-css.css";
+import { isSunday } from "date-fns";
 
 function Homepage() {
 
     const [value, setValue] = React.useState(new Date());
+    const [seanShame, setSeanShame] = React.useState(0);
+    const [steveShame, setSteveShame] = React.useState(0);
 
     return (
         <div>
-            <h1>
-                ALL ABOARD THE PAIN TRAIN
+            <h1 id = "header">
+                2022 Fitness Challenge
             </h1>
             <div id = "date">
                 <LocalizationProvider dateAdapter={AdapterDateFns}>
@@ -23,7 +26,7 @@ function Homepage() {
                         orientation="landscape"
                         openTo="day"
                         value={value}
-                        shouldDisableDate={isWeekend}
+                        shouldDisableDate={isSunday}
                         onChange={(newValue) => {
                         setValue(newValue);
                         }}
@@ -31,15 +34,26 @@ function Homepage() {
                     />
                 </LocalizationProvider>
             </div>
+            
 
-            <div id = "check_in_buttons">
-                <h2 class = "nametag">
-                    Sean
-                </h2>
-                <h2 class = "nametag">
-                    Steve
-                </h2>
-            </div>
+            <Grid id = "buttonContainer" container spacing = {2} justifyContent = "flex-end">
+                <Grid item xs = {3}>
+                    <h1>Sean</h1>
+                    <ButtonGroup class = "buttons">
+                        <Button style={{ fontSize: '70px', background: 'gray', margin: '10px'  }}  variant = "outlined" color = "primary">✔</Button>
+                        <Button style={{ fontSize: '70px', background: 'gray', margin: '10px'  }} size="humongous" variant = "outlined" color = "error">✘</Button>
+                    </ButtonGroup>
+                    
+                </Grid>
+
+                <Grid item xs = {3}>
+                    <h1>Steve</h1>
+                    <ButtonGroup class = "buttons">
+                        <Button style={{ fontSize: '70px', background: 'gray', margin: '10px' }} variant = "outlined" color = "primary">✔</Button>
+                        <Button style={{ fontSize: '70px', background: 'gray', margin: '10px'  }} size="humongous" variant = "outlined" color = "error">✘</Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
             
 
         </div>
